7ff08f1f1a6dff896475d0e5da24319c99b729cc
Aford <244734834@qq.com>
2018-01-31 13:38:51 +0800

    增加换肤

--- js/init.js
+++ js/init.js
@@ -3,5 +3,9 @@ var setClient = function () {
   var clientWidth = doc.clientWidth;
   doc.style.fontSize = 100 * clientWidth / 750 + 'px';
 }
+window.changeTheme = function () {
+  document.getElementById('profile').setAttribute('href', '../css/main-light.css');
+  window.globalThemeType = 'light';
+}
 setClient()
 window.addEventListener('resize', setClient, false)
