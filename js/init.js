d930fac113ae192c615c80b1f6e584e6009a31c4
OwlAford <244734834@qq.com>
2018-09-19 10:56:21 +0800

    📦 update

--- js/init.js
+++ js/init.js
@@ -3,9 +3,19 @@ var setClient = function () {
   var clientWidth = doc.clientWidth;
   doc.style.fontSize = 100 * clientWidth / 750 + 'px';
 }
-window.changeTheme = function () {
-  document.getElementById('profile').setAttribute('href', '../css/main-light.css');
-  window.globalThemeType = 'light';
+
+window.globalThemeType = "dark";
+
+window.changeTheme = function (notLight) {
+  var cssfile = document.getElementById('profile');
+  if (notLight) {
+    cssfile.setAttribute('href', '../css/main.css');
+    window.globalThemeType = 'dark';
+  } else {
+    cssfile.setAttribute('href', '../css/main-light.css');
+    window.globalThemeType = 'light';
+  }
 }
+
 setClient()
 window.addEventListener('resize', setClient, false)
